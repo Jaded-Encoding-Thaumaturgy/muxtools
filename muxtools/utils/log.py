0c38541facb0b3d8bd5edd11e524f8c79fa75f55
petzku <petzku@zku.fi>
2024-02-11 01:33:16 +0200

    log: Add custom exception to be returned from log.error and log.crit

--- muxtools/utils/log.py
+++ muxtools/utils/log.py
@@ -3,7 +3,12 @@ from rich.logging import RichHandler
 import time
 import inspect
 
-__all__ = ["crit", "debug", "error", "exit", "info", "warn", "logger"]
+__all__ = ["crit", "debug", "error", "exit", "info", "warn", "logger", "LoggingException"]
+
+
+class LoggingException(Exception):
+    """Custom exception returned from log.crit and log.error"""
+
 
 FORMAT = "%(name)s | %(message)s"  #
 logging.basicConfig(format=FORMAT, datefmt="[%X]", handlers=[RichHandler(markup=True, omit_repeated_times=False, show_path=False)])
@@ -19,10 +24,10 @@ def _format_msg(msg: str, caller: any) -> str:
     return msg if caller is None else f"[bold]{caller}:[/] {msg}"
 
 
-def crit(msg: str, caller: any = None) -> Exception:
+def crit(msg: str, caller: any = None) -> LoggingException:
     message = _format_msg(msg, caller)
     logger.critical(message)
-    return Exception(message)
+    return LoggingException(message)
 
 
 def debug(msg: str, caller: any = None):
@@ -46,10 +51,10 @@ def warn(msg: str, caller: any = None, sleep: int = 0):
         time.sleep(sleep)
 
 
-def error(msg: str, caller: any = None) -> Exception:
+def error(msg: str, caller: any = None) -> LoggingException:
     message = _format_msg(msg, caller)
     logger.error(message)
-    return Exception(message)
+    return LoggingException(message)
 
 
 def exit(msg: str, caller: any = None):
