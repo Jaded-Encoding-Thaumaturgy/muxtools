2997e1624d5caff41d3c38937a85debd2be128db
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-04-23 22:14:10 +0200

    tests: Add fixture and move cwd out of the test

--- tests/test_sub_shifting.py
+++ tests/test_sub_shifting.py
@@ -2,6 +2,20 @@ from muxtools import SubFile, VideoMeta, Setup, get_workdir, ensure_path
 from muxtools.subtitle.basesub import _Line
 from typing import cast
 from shutil import rmtree
+from time import sleep
+import pytest
+
+test_dir = ensure_path(__file__, None).parent
+
+
+@pytest.fixture(autouse=True)
+def setup_and_remove():
+    Setup("Test", None)
+
+    yield
+
+    sleep(0.5)  # Just in case there's still a lock on some file lol
+    rmtree(get_workdir())
 
 
 def test_shift_by_24() -> None:
@@ -11,17 +25,13 @@ def test_shift_by_24() -> None:
     The "output" file to compare with was created by opening the "input" file in aegisub
     with the same video that was used to generate the json VideoMeta.
     """
-    Setup("Test", None)
+    ts = VideoMeta.from_json(test_dir / "test-data" / "input" / "vigilantes_s01e01.json")
 
-    cwd = ensure_path(__file__, test_shift_by_24).parent
-
-    ts = VideoMeta.from_json(cwd / "test-data" / "input" / "vigilantes_s01e01.json")
-
-    sub = SubFile(cwd / "test-data" / "input" / "vigilantes_s01e01_en.ass")
+    sub = SubFile(test_dir / "test-data" / "input" / "vigilantes_s01e01_en.ass")
     sub.shift(24, ts)
     sub_doc = sub._read_doc()
 
-    sub_correct = SubFile(cwd / "test-data" / "output" / "vigilantes_s01e01_en_shifted.ass")
+    sub_correct = SubFile(test_dir / "test-data" / "output" / "vigilantes_s01e01_en_shifted.ass")
     sub_correct_doc = sub_correct._read_doc()
 
     for presumed, correct in zip(sub_doc.events, sub_correct_doc.events):
@@ -30,5 +40,3 @@ def test_shift_by_24() -> None:
 
         assert presumed.start == correct.start
         assert presumed.end == correct.end
-
-    rmtree(get_workdir())
