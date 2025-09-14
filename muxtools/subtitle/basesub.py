16307e14365af8324abc6c6d86bfde21963ffe48
Alex H. <48185123+Vodes@users.noreply.github.com>
2025-09-14 23:03:24 +0200

    manipulate_lines: Explicitly check for None to allow empty list

--- muxtools/subtitle/basesub.py
+++ muxtools/subtitle/basesub.py
@@ -177,7 +177,7 @@ class BaseSubFile(ABC, MuxingFile):
     def manipulate_lines(self, func: Callable[[list[_Line]], list[_Line] | None]) -> None:
         doc = self._read_doc()
         returned = func(doc.events)  # type: ignore
-        if returned:
+        if returned is not None:
             doc.events = returned
         self._update_doc(doc)
 
